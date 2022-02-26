4d3f2b5148ef90230e8b9d963ab03d58fe4d62bb
hyemin <hmpkk09@gmail.com>
2022-02-27 02:14:29 +0900

    feat: config 모듈 구현

---
+++ src/config/index.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getEnv = <T>(key: string, defaultValue?: T): T | string => {
+  const value = process.env[key] || defaultValue;
+
+  if (value == null) {
+    throw new Error('환경 변수가 없습니다.');
+  }
+
+  return value;
+};
+
+type Config = {
+  db: {
+    host: string;
+  };
+  port: number;
+  bcrypt: {
+    saltRound: number;
+  };
+  jwt: {
+    secretKey: string;
+    expiresIn: string;
+  };
+};
+
+const config: Config = {
+  db: {
+    host: getEnv('DB_HOST'),
+  },
+  port: Number(getEnv('PORT', 8080)),
+  bcrypt: {
+    saltRound: Number(getEnv('BCRYPT_SALT_ROUND', 8)),
+  },
+  jwt: {
+    secretKey: getEnv('JWT_SECRET_KEY'),
+    expiresIn: getEnv('JWT_EXPIRES_IN'),
+  },
+};
+
+export default config;
