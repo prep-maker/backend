6e9092cf8bf13f0e802b9d369f8abefeb1610776
hyemin <hmpkk09@gmail.com>
2022-02-27 10:59:03 +0900

    feat: POST /auth/signin 라우터 추가

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import AuthController from '../controllers/auth.js';
-import { signupValidators } from '../middlewares/validators/index.js';
+import {
+  signinValidators,
+  signupValidators,
+} from '../middlewares/validators/index.js';
 import userModel from '../models/user.js';
 import AuthService from '../services/auth.js';
 
@@ -10,5 +13,6 @@ const authService = new AuthService(userModel);
 const authController = new AuthController(authService);
 
 router.post('/signup', signupValidators, authController.signup);
+router.post('/signin', signinValidators, authController.signin);
 
 export default router;
