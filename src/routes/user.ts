68e6c61bd8f0128d03504a2b7d7d3bc9f9164bf2
hyemin <hmpkk09@gmail.com>
2022-03-01 02:11:49 +0900

    feat: GET /users/:userId/writings 라우터 추가

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import WritingController from '../controllers/writing.js';
+import { ValidatorsForGetWritings } from '../middlewares/validators/index.js';
+import writingModel from '../models/writing.js';
+import WritingService from '../services/writing.js';
 
 const router = express.Router();
 
-router.get('/:id/writings');
+const writingService = new WritingService(writingModel);
+const writingController = new WritingController(writingService);
+
+router.get(
+  '/:userId/writings',
+  ValidatorsForGetWritings,
+  writingController.getWritings
+);
 
 export default router;
