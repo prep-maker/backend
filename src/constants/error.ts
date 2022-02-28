51c24b78b68aed96b17efd6b12c3b694191f4612
hyemin <hmpkk09@gmail.com>
2022-03-01 00:42:31 +0900

    feat: 에러 상수 메세지 추가

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -3,4 +3,6 @@ export const ERROR = {
   NOT_FOUND_USER: '유저를 찾을 수 없습니다.',
   INVALID_LOGIN: '이메일 혹은 비밀번호가 잘못되었습니다.',
   INVALID_ID: '잘못된 형식의 id입니다.',
+  INVALID_WIRINTG_QUERY: 'query는 editing, done 중에 하나여야 합니다.',
+  UNKNOWN_ERROR: '알수없는 에러',
 } as const;
