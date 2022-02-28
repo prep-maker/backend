20e8044e0d33d37e020cc1ebccd9694a037a41e9
hyemin <hmpkk09@gmail.com>
2022-02-28 18:20:42 +0900

    feat: ERROR 상수에 메세지 추가

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -2,4 +2,5 @@ export const ERROR = {
   DUPLICATE_EMAIL: '이미 존재하는 이메일입니다.',
   NOT_FOUND_USER: '유저를 찾을 수 없습니다.',
   INVALID_LOGIN: '이메일 혹은 비밀번호가 잘못되었습니다.',
+  INVALID_ID: '잘못된 형식의 id입니다.',
 } as const;
