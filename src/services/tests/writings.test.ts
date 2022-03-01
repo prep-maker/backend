682b24d67063926e32d1bf51084046e26f830476
hyemin <hmpkk09@gmail.com>
2022-03-01 22:08:56 +0900

    test: ts-mockito stub을 커스텀 stub으로 변경

--- src/services/tests/writings.test.ts
+++ src/services/tests/writings.test.ts
@@ -1,55 +1,22 @@
 import mongoose from 'mongoose';
-import { deepEqual, instance, mock, when } from 'ts-mockito';
 import { ERROR } from '../../constants/error';
 import dummyWritings from '../../fixtures/dummyWritings';
-import { WritingModel } from '../../types/writing';
+import UserModelStub from '../../fixtures/userModelStub';
+import WritingModelStub from '../../fixtures/writingModelStub';
 import { useFailState, useSuccessState } from '../../utils/state';
 import WritingService from '../writing';
 
 describe('WritingService', () => {
   let writingService: WritingService;
   const userId = '621cafb14ed8fbc8812e845c';
-  const objectId = mongoose.Types.ObjectId(userId);
   beforeEach(() => {
-    const modelStub: WritingModel = mock();
-    when(modelStub.findAllByUserId(deepEqual(objectId))).thenResolve(
-      dummyWritings as any
+    writingService = new WritingService(
+      new WritingModelStub(),
+      new UserModelStub()
     );
-    when(modelStub.findDoneByUserId(deepEqual(objectId))).thenResolve(
-      dummyWritings.filter((writing) => writing.isDone) as any
-    );
-    when(modelStub.findEditingByUserId(deepEqual(objectId))).thenResolve(
-      dummyWritings.filter((writing) => !writing.isDone) as any
-    );
-    when(
-      modelStub.create(
-        deepEqual({
-          isDone: false,
-          author: objectId,
-          title: 'Untitled',
-          blocks: [],
-        })
-      )
-    ).thenResolve({
-      _id: mongoose.Types.ObjectId('621cb0b250e465dfac337175'),
-      isDone: false,
-      author: userId,
-      title: 'Untitled',
-      blocks: [],
-    } as any);
-    writingService = new WritingService(instance(modelStub));
   });
 
   describe('getByUserIdAndState', () => {
-    it('잘못된 형식의 userId가 입력되면 status 400의 FailState를 리턴한다.', async () => {
-      const result = await writingService.getByUserIdAndState(
-        'invalidId',
-        undefined
-      );
-
-      expect(result).toEqual(useFailState(ERROR.INVALID_ID, 400));
-    });
-
     it('userId가 입력되면 author와 userId가 매치되는 모든 다큐먼트를 SuccessState로 리턴한다.', async () => {
       const result = await writingService.getByUserIdAndState(
         userId,
@@ -73,6 +40,15 @@ describe('WritingService', () => {
       expect(result).toEqual(
         useSuccessState(dummyWritings.filter((writing) => writing.isDone))
       );
+    });
+
+    it('잘못된 형식의 userId가 입력되면 status 400의 FailState를 리턴한다.', async () => {
+      const result = await writingService.getByUserIdAndState(
+        'invalidId',
+        undefined
+      );
+
+      expect(result).toEqual(useFailState(ERROR.INVALID_ID, 400));
     });
   });
 
