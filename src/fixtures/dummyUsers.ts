2fedec293f65c4d51d2570d9ce64c28977bc2314
hyemin <hmpkk09@gmail.com>
2022-03-01 21:52:07 +0900

    feat: dummyUser 파일 작성

---
+++ src/fixtures/dummyUsers.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: mongoose.Types.ObjectId('621cafb14ed8fbc8812e845c'),
+    email: 'test1@email.com',
+    name: 'test1',
+    password: 'test1234',
+    writings: [],
+  },
+  {
+    _id: mongoose.Types.ObjectId('621dfdcec156df650b5f6e74'),
+    email: 'test2@email.com',
+    name: 'test2',
+    password: 'test1234',
+    writings: [],
+  },
+];
