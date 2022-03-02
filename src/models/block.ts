7831192e83b49098c16b1d5d89e403921f2a0135
hyemin <hmpkk09@gmail.com>
2022-03-02 13:22:35 +0900

    feat: blockModel에 deleteByIds 메소드 추가

--- src/models/block.ts
+++ src/models/block.ts
@@ -1,3 +1,4 @@
+import { concurrent, map, pipe, toAsync } from '@fxts/core';
 import mongoose from 'mongoose';
 import { BlockModel, BlockSchema, ParagraphSchema } from '../types/block.js';
 
@@ -26,6 +27,17 @@ const blockSchema: mongoose.Schema<BlockSchema> = new mongoose.Schema({
     type: [paragraphSchema],
   },
 });
+
+blockSchema.statics.deleteByIds = async function (
+  ids: readonly mongoose.Types.ObjectId[]
+): Promise<void> {
+  pipe(
+    ids,
+    toAsync,
+    map(this.findByIdAndRemove.bind(blockModel)),
+    concurrent(5)
+  );
+};
 
 const blockModel = mongoose.model<BlockSchema, BlockModel>(
   'block',
