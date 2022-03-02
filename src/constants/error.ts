f33911ad24c3409b77ebe4ce2a56f4af606aa513
hyemin <hmpkk09@gmail.com>
2022-03-02 13:59:22 +0900

    feat: ERROR 상수 메세지 추가

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,8 +1,12 @@
 export const ERROR = {
   DUPLICATE_EMAIL: '이미 존재하는 이메일입니다.',
   NOT_FOUND_USER: '유저를 찾을 수 없습니다.',
+  NOT_FOUND_WRITING: '글을 찾을 수 없습니다.',
+  NOT_FOUND_BLOCK: '블록을 찾을 수 없습니다.',
   INVALID_LOGIN: '이메일 혹은 비밀번호가 잘못되었습니다.',
   INVALID_USER_ID: '잘못된 형식의 user id입니다.',
+  INVALID_WRITING_ID: '잘못된 형식의 writing id입니다.',
+  INVALID_BLOCK_ID: '잘못된 형식의 block id입니다.',
   INVALID_WIRINTG_QUERY: 'query는 editing, done 중에 하나여야 합니다.',
   UNKNOWN_ERROR: '알수없는 에러',
 } as const;
