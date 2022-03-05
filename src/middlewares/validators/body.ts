c8c351675bc4079fa27aea4542b4e43ad0da9c0b
hyemin <hmpkk09@gmail.com>
2022-03-05 18:34:35 +0900

    fix: auth validator 메세지 형식 수정

--- src/middlewares/validators/body.ts
+++ src/middlewares/validators/body.ts
@@ -32,42 +32,48 @@ export const signinBodyChain = [
   body('email')
     .trim()
     .notEmpty()
-    .withMessage('email을 입력해주세요.')
+    .withMessage({ message: 'email을 입력해주세요.', status: 400 })
     .normalizeEmail()
     .isEmail()
-    .withMessage('잘못된 이메일 형식입니다.'),
+    .withMessage({ message: '잘못된 이메일 형식입니다.', status: 400 }),
   body('password')
+    .isString()
+    .withMessage({ message: '비밀번호는 문자열이어야 합니다.', status: 400 })
     .trim()
     .notEmpty()
-    .withMessage('비밀번호를 입력해주세요.')
-    .isString()
-    .withMessage('비밀번호는 문자열이어야 합니다.')
+    .withMessage({ message: '비밀번호를 입력해주세요.', status: 400 })
     .isLength({ min: 6, max: 20 })
-    .withMessage('비밀번호는 6자 이상 20자 이하여야 합니다.'),
+    .withMessage({
+      message: '비밀번호는 6자 이상 20자 이하여야 합니다.',
+      status: 400,
+    }),
 ];
 
 export const signupBodyChain = [
   body('email')
     .trim()
     .notEmpty()
-    .withMessage('email을 입력해주세요.')
+    .withMessage({ message: 'email을 입력해주세요.', status: 400 })
     .normalizeEmail()
     .isEmail()
-    .withMessage('잘못된 이메일 형식입니다.'),
+    .withMessage({ message: '잘못된 이메일 형식입니다.', status: 400 }),
   body('name')
+    .isString()
+    .withMessage({ message: 'name은 문자열이어야 합니다.', status: 400 })
     .trim()
     .notEmpty()
-    .withMessage('name을 입력해주세요.')
-    .isString()
-    .withMessage('name은 문자열이어야 합니다.')
+    .withMessage({ message: 'name을 입력해주세요.', status: 400 })
     .isLength({ max: 10 })
-    .withMessage('이름은 10자 이하여야 합니다.'),
+    .withMessage({ message: '이름은 10자 이하여야 합니다.', status: 400 }),
   body('password')
+    .isString()
+    .withMessage({ message: '비밀번호는 문자열이어야 합니다.', status: 400 })
     .trim()
     .notEmpty()
-    .withMessage('비밀번호를 입력해주세요.')
-    .isString()
-    .withMessage('비밀번호는 문자열이어야 합니다.')
+    .withMessage({ message: '비밀번호를 입력해주세요.', status: 400 })
     .isLength({ min: 6, max: 20 })
-    .withMessage('비밀번호는 6자 이상 20자 이하여야 합니다.'),
+    .withMessage({
+      message: '비밀번호는 6자 이상 20자 이하여야 합니다.',
+      status: 400,
+    }),
 ];
