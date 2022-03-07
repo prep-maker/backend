77ac7e9e31453f151db0727e38f7cc94c6766c91
hyemin <hmpkk09@gmail.com>
2022-03-07 14:07:11 +0900

    feat: blockService에 update 메소드 추가

--- src/services/block.ts
+++ src/services/block.ts
@@ -2,17 +2,19 @@ import mongoose from 'mongoose';
 import {
   BlockDocument,
   BlockRepository,
+  BlockResponse,
   BlockSchema,
 } from '../common/types/block.js';
-import { ObjectId } from '../common/types/mongoose.js';
 import { WritingRepository } from '../common/types/writing.js';
 import { useSuccessState } from '../common/utils/state.js';
 
-type BlockResult = Promise<SuccessState<BlockSchema & { id: ObjectId }>>;
+type BlockResult = Promise<SuccessState<BlockResponse>>;
+type BlockListResult = Promise<SuccessState<BlockResponse[]>>;
 
 export interface IBlockService {
   create: (writingId: string, block: BlockSchema) => BlockResult;
   remove: (writingId: string, blockId: string) => Promise<void>;
+  update: (writingId: string, blocks: BlockSchema[]) => BlockListResult;
 }
 
 class BlockService implements IBlockService {
@@ -20,6 +22,23 @@ class BlockService implements IBlockService {
     private readonly blockModel: BlockRepository,
     private readonly writingModel: WritingRepository
   ) {}
+
+  update = async (
+    writingId: string,
+    blocks: BlockSchema[]
+  ): BlockListResult => {
+    const newBlocks = await this.blockModel.createBlocks(blocks);
+    const blockIds = newBlocks.map((block) => block.id);
+    const writing = await this.writingModel.updateById(
+      writingId,
+      { blocks: blockIds },
+      { new: false }
+    );
+    const oldBlocks = writing.blocks.map((block) => block._id);
+    await this.blockModel.deleteByIds(oldBlocks);
+
+    return useSuccessState(newBlocks);
+  };
 
   create = async (writingId: string, block: BlockSchema): BlockResult => {
     const newBlock: BlockDocument = await this.blockModel.create(block);
