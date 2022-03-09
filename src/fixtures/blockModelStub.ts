219003629712cd47bba978d472188e0347b23cea
hyemin <hmpkk09@gmail.com>
2022-03-09 21:04:00 +0900

    feat blockModelStub 업데이트

--- src/fixtures/blockModelStub.ts
+++ src/fixtures/blockModelStub.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import {
   BlockDocument,
   BlockRepository,
+  BlockResponse,
   BlockSchema,
 } from '../common/types/block';
 import { ObjectId } from '../common/types/mongoose';
@@ -9,6 +10,13 @@ import { ObjectId } from '../common/types/mongoose';
 export const BLOCK_ID = mongoose.Types.ObjectId('621ee328d1172c53545dee69');
 
 class BlockModelStub implements BlockRepository {
+  updateById = async (
+    blockId: string,
+    block: BlockSchema
+  ): Promise<BlockResponse> => ({
+    ...block,
+    id: blockId,
+  });
   create = async (block: BlockSchema): Promise<BlockDocument> =>
     ({
       _id: BLOCK_ID,
