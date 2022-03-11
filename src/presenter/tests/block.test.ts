4a8b006617fdb794eaede6c0145046603609a93b
hyemin <hmpkk09@gmail.com>
2022-03-11 11:42:23 +0900

    test: BlockPresenter update 메소드 테스트

--- src/presenter/tests/block.test.ts
+++ src/presenter/tests/block.test.ts
@@ -12,6 +12,7 @@ describe('BlockPresenter', () => {
   let blockPresenter: IBlockPresenter;
   let blockModel: BlockRepository;
   let writingModel: WritingRepository;
+  const INVALID_ID = 'invalid id';
   beforeEach(() => {
     blockModel = new BlockModelStub();
     writingModel = new WritingModelStub();
@@ -42,7 +43,7 @@ describe('BlockPresenter', () => {
     });
 
     it('잘못된 형식의 writingId가 입력되면 status 400의 FailState를 리턴한다.', async () => {
-      const result = await blockPresenter.create('invalid id', newBlock);
+      const result = await blockPresenter.create(INVALID_ID, newBlock);
 
       expect(result).toEqual(useFailState(ERROR.INVALID_WRITING_ID, 400));
     });
@@ -63,18 +64,42 @@ describe('BlockPresenter', () => {
 
     it('잘못된 형식의 writingId나 blockId가 입력되면 status 400의 FailState를 리턴한다.', async () => {
       const invalidWritingId = await blockPresenter.remove(
-        'invalid id',
+        INVALID_ID,
         BLOCK_ID
       );
       const invalidBlockId = await blockPresenter.remove(
         WRITING_ID,
-        'invalid id'
+        INVALID_ID
       );
 
       expect(invalidWritingId).toEqual(
         useFailState(ERROR.INVALID_WRITING_ID, 400)
       );
       expect(invalidBlockId).toEqual(useFailState(ERROR.INVALID_BLOCK_ID, 400));
+    });
+  });
+
+  describe('update', () => {
+    it('blockId와 업데이트될 block이 입력되면 업데이트 된 내용의 block을 SuccessState로 리턴한다.', async () => {
+      const updated: BlockSchema = {
+        type: 'P',
+        paragraphs: [{ type: 'P', content: 'text' }],
+      };
+
+      const result = await blockPresenter.update(BLOCK_ID, updated);
+
+      expect(result).toEqual(useSuccessState({ ...updated, id: BLOCK_ID }));
+    });
+
+    it('잘못된 형식의 blockId가 입력되면 status 400의 FailState를 리턴한다..', async () => {
+      const updated: BlockSchema = {
+        type: 'P',
+        paragraphs: [{ type: 'P', content: 'text' }],
+      };
+
+      const result = await blockPresenter.update(INVALID_ID, updated);
+
+      expect(result).toEqual(useFailState(ERROR.INVALID_BLOCK_ID, 400));
     });
   });
 });
